3c54a089eba5b8db75dd643e5cbe02a934d5fda7
Rafael Medina <rafamedgar@gmail.com>
2019-11-09 12:38:46 +0100

    Added README

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='Flask-Discussion',
     version='0.1.0',
 
     description='Comment system integration for Flask applications',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     url='https://github.com/rmed/flask-discussion',
 
@@ -14,15 +18,19 @@ setup(
 
     license='MIT',
 
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'
+    ],
 
     keywords='flask discussion comments commenting',
 
     packages=find_packages(),
 
     include_package_data=True,
-
-    zip_safe=False,
 
     install_requires=[
         'Flask>=1.1.1',
